5f2db58f660bbad8665f679729589e37259e4071
莫非 <313303303@qq.com>
2018-03-07 12:45:52 +0800

    Update utils.py

--- 3_Pyspark/utils.py
+++ 3_Pyspark/utils.py
@@ -2,9 +2,9 @@
 def get_schema(num_col_names, str_col_names):
     assert isinstance(num_col_names[0], str)
     assert isinstance(str_col_names[0], str)
-    struct = StructType()
+    _struct = StructType()
     for i in num_col_names:
-        struct.add(i, FloatType())
+        _struct.add(i, FloatType())
     for i in str_col_names:
-        struct.add(i, StringType())
-    return struct
+        _struct.add(i, StringType())
+    return _struct
