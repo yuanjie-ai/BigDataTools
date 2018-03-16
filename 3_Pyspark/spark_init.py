7e1f00ed8bc03017a49606e653abf5d37124dc16
莫非 <313303303@qq.com>
2018-03-16 11:55:05 +0800

    Create spark_init.py

---
+++ 3_Pyspark/spark_init.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = 'spark_init'
+__author__ = 'JieYuan'
+__mtime__ = '2018/3/16'
+"""
+import os
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# Path for spark source folder
+os.environ['SPARK_HOME'] = "/opt/BigData/spark"
+
+# # Path for pyspark and py4j
+# sys.path.append("/opt/BigData/spark/python")
+# sys.path.append("/opt/BigData/spark/python/lib/py4j-0.10.4-src.zip")
+
+## You might need to enter your local IP
+# os.environ['SPARK_LOCAL_IP']="192.168.2.138"
+
+# Init
+spark = SparkSession.builder \
+    .appName("Yuanjie") \
+    .config('log4j.rootCategory', "WARN") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+# 动态分区
+# spark.sql("set hive.exec.dynamic.partition.mode = nonstrict")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SPARK = \
+    '''
+          ____              __
+         / __/__  ___ _____/ /__
+        _\ \/ _ \/ _ `/ __/  '_/
+       /___/ .__/\_,_/_/ /_/\_\    >>>https://github.com/Jie-Yuan
+          /_/
+
+    '''
+if __name__ == '__main__':
+    print('It is main!!!')
+else:
+    print(SPARK)
